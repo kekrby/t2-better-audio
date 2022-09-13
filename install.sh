89835d4fae456db464d73359e3f5801ba41359be
Aditya Garg <85610623+AdityaGarg8@users.noreply.github.com>
2022-09-13 21:03:11 +0530

    Fix permission error on running bash script and make it verbose

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env sh
 
-cp files/alsa-card-configs/* /usr/share/alsa/cards/
-cp files/profile-sets/* /usr/share/alsa-card-profile/mixer/profile-sets/
-cp files/91-pulseaudio-custom.rules /usr/lib/udev/rules.d/
+if [ $USER != root ]
+then
+sudo chmod 755 $0
+sudo $0
+exit 0
+fi
+
+cp -v files/alsa-card-configs/* /usr/share/alsa/cards/
+cp -v files/profile-sets/* /usr/share/alsa-card-profile/mixer/profile-sets/
+cp -v files/91-pulseaudio-custom.rules /usr/lib/udev/rules.d/
