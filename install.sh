4fed5a0b277cb7aada33efa907c7f8fb7dea2cbf
Aditya Garg <85610623+AdityaGarg8@users.noreply.github.com>
2022-09-25 21:35:36 +0530

    install.sh: ask if the user wants to remove old configuration files if they are present
    Co-authored-by: kekrby <kekrby@gmail.com>

--- install.sh
+++ install.sh
@@ -1,12 +1,52 @@
 #!/usr/bin/env sh
 
+# This script must be run as a normal user as it needs to read the XDG_CONFIG_HOME variable.
+# The user will be asked for their password after saving the variable to a file in /tmp.
+
 if [ $USER != root ]
 then
     echo This script must be run as root, you will be prompted for your password
+    echo ${XDG_CONFIG_HOME:-~/.config} | sudo tee /tmp/user.txt
     sudo chmod 755 $0
     sudo ./$0
+    sudo rm /tmp/user.txt
     exit 0
 fi
+
+continue=unset
+
+for file in "/usr/share/pulseaudio/alsa-mixer/profile-sets/apple-t2.conf" \
+            "/usr/share/alsa-card-profile/mixer/profile-sets/apple-t2.conf" \
+            "/usr/lib/udev/rules.d/91-pulseaudio-custom.rules" \
+            "/usr/share/alsa/cards/AppleT2.conf"
+do
+    if [ -f "$file" ]
+    then
+        if [ "$continue" = "unset" ]
+        then
+            read -p "Old configuration files are present, do you want to delete them? (y/n) " continue
+
+            if [ "$continue" != "y" ]
+            then
+                break
+            fi
+        fi
+
+        rm -v $file
+    fi
+done
+
+config_home=$(cat /tmp/user.txt)
+
+for file in "$config_home/pulse/default.pa" \
+            "$config_home/pulse/daemon.conf"
+do
+    if [ -f "$file" ]
+    then
+        echo "You seem to have pulseaudio configuration files present, don't forget to update them if they contain anything specific to T2 Macs"
+        break
+    fi
+done
 
 for dir in "/usr/share/alsa-card-profile/mixer" "/usr/share/pulseaudio/alsa-mixer"
 do
